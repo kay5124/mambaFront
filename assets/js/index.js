b38a60b69413c920e2d1811ff19b9e17c7623031
kay5124 <dsa123465@gmail.com>
2019-08-06 14:53:54 +0800

    install sass and finished header and footer

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,28 @@
-
-function InitHead(){
+function InitHead() {
     $.ajax({
         url: "./templete/header/head.html",
         dataType: "html",
         success: function(data) {
             $("#header").html(data);
+            $("head").append('<link rel="stylesheet" type="text/css" href="./assets/scss/header/head.css" />');
         },
-        error: function(err){
+        error: function(err) {
+            console.log(err);
+        }
+    })
+}
+
+function InitFoot() {
+    $.ajax({
+        url: "./templete/footer/footer.html",
+        dataType: "html",
+        success: function(data) {
+            $("#footer").html(data);
+            $("head").append('<link rel="stylesheet" type="text/css" href="./assets/scss/footer/foot.css" />');
+        },
+        error: function(err) {
             console.log(err);
         }
     })
 
-    $.ajax({
-        url: href,
-        dataType: 'css',
-        success: function(){                  
-                $('<link rel="stylesheet" type="text/css" href="'+href+'" />').appendTo("head");
-                //your callback
-            }
-    })
 }
-
