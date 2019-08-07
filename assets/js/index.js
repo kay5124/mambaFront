da0e0419d4ceafea6189f358bd351c4bff0faee8
kay5124 <dsa123465@gmail.com>
2019-08-07 14:08:36 +0800

    finish index~~

--- assets/js/index.js
+++ assets/js/index.js
@@ -26,20 +26,14 @@ function InitFoot() {
     })
 }
 
-function InitMainPage(){
+function InitMainPage() {
     $.ajax({
         url: "./templete/Page/home.html",
         dataType: "html",
         success: function(data) {
             $("#mainContent").html(data);
             $("head").append('<link rel="stylesheet" type="text/css" href="./assets/scss/home/home.css" />');
-
-            $('.fadeOut').owlCarousel({
-                items: 1,
-                animateOut: 'fadeOut',
-                loop: true,
-                margin: 10,
-            });
+            $('.carousel').carousel();
         },
         error: function(err) {
             console.log(err);
