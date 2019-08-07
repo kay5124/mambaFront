c6ff2fc6b01e41f7f29c566e7d42caf06e36336d
kay5124 <dsa123465@gmail.com>
2019-08-07 17:54:09 +0800

    big update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,42 +1,41 @@
+function directUrl(route) {
+    if (route == "login" || route == "register") route = "Member/" + route;
+
+    if (route != "/") {
+        window.location.href = route;
+    } else {
+        window.location.href = "/";
+    }
+
+}
+
+// function InitMeta() {
+//     $.ajax({
+//         url: '/templete/headModule.html',
+//         dataType: "html",
+//         success: function(data) {
+//             $("head").append(data);
+//         }
+//     })
+// }
+
 function InitHead() {
-    $.ajax({
-        url: "./templete/header/head.html",
-        dataType: "html",
-        success: function(data) {
-            $("#header").html(data);
-            $("head").append('<link rel="stylesheet" type="text/css" href="./assets/scss/header/head.css" />');
-        },
-        error: function(err) {
-            console.log(err);
-        }
-    })
+    $("#header").load("/templete/header/head.html");
+}
+
+function InitHead_OnlyLogo() {
+    $("#header").load("/templete/header/head_onlylogo.html");
 }
 
 function InitFoot() {
-    $.ajax({
-        url: "./templete/footer/footer.html",
-        dataType: "html",
-        success: function(data) {
-            $("#footer").html(data);
-            $("head").append('<link rel="stylesheet" type="text/css" href="./assets/scss/footer/foot.css" />');
-        },
-        error: function(err) {
-            console.log(err);
-        }
-    })
+    $("#footer").load("/templete/footer/footer.html");
+}
+
+function InitBanner() {
+    $("#banner").load("/templete/main/banner.html");
+    $('.carousel').carousel();
 }
 
 function InitMainPage() {
-    $.ajax({
-        url: "./templete/Page/home.html",
-        dataType: "html",
-        success: function(data) {
-            $("#mainContent").html(data);
-            $("head").append('<link rel="stylesheet" type="text/css" href="./assets/scss/home/home.css" />');
-            $('.carousel').carousel();
-        },
-        error: function(err) {
-            console.log(err);
-        }
-    })
+    $(".main").load("/templete/Page/storeGameList/storeGameList.html");
 }
